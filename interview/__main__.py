c858004f5a74a3bc7c88611ca0703d47da2658a5
Nathaniel Cogneaux <Nathaniel@cogneaux.net>
2024-11-06 18:48:26 +0900

    Cleaning

--- interview/__main__.py
+++
@@ -1,4 +0,0 @@
-import sys
-from interview import weather
-
-weather.process_csv(sys.stdin, sys.stdout)
--- interview/weather.py
+++
@@ -1,3 +0,0 @@
-
-def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
